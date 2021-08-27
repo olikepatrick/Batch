bdd6661e2d28811377d47b5992e52e51633841e1
olikepatrick <nina1999210@gmail.com>
2021-08-27 14:09:36 +0800

    basic functions

---
+++ inDevelopment/gitPull.bat
@@ -0,0 +1,27 @@
+@echo off
+
+@REM cd .\onward\OWSF
+
+goto start
+
+:start
+    echo 1. OWSF(mine)
+    echo 2. OWSF_Platform(chris)
+    echo 3. Batch
+    set /p choosen="which you want to update from github :"
+
+    if %choosen% == 1 (
+        cd C:\Users\nina1\Desktop\onward\github\OWSF
+        goto :pull
+    ) else if %choosen% == 2 (
+        cd C:\Users\nina1\Desktop\onward\github\OWSF_Platform
+        goto :pull
+    ) else if %choosen% == 3 (
+        cd C:\Users\nina1\Desktop\Batch
+        goto :pull
+    ) else (
+        goto start
+    )
+
+:pull
+    echo ============================= pulling ==============================
